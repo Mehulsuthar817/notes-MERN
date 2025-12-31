22eeea4737e902f8a424c30693b6c9631cbfa712
Mehulsuthar817 <mehulsuthar817@gmail.com>
2025-12-31 16:35:21 +0530

    fix axios baseURL usage

--- notes-frontend/src/api/notesAPI.js
+++ notes-frontend/src/api/notesAPI.js
@@ -1,9 +1,16 @@
 import api from "./axios";
 
-const API = "http://localhost:5000";
+export const getNotes = () =>
+  api.get("/notes").then(res => ({
+    ...res,
+    data: res.data.map(n => ({ ...n, id: n._id })),
+  }));
 
+export const addNote = (content) =>
+  api.post("/notes", { content });
 
-export const getNotes = () => api.get(`${API}/notes`).then(res => ({ ...res, data: res.data.map(n => ({ ...n, id: n._id })) }));
-export const addNote = (content) => api.post(`${API}/notes`, { content });
-export const updateNote = (id,content)=>api.put(`${API}/notes/${id}`,{content});
-export const deleteNote = (id)=>api.delete(`${API}/notes/${id}`);
+export const updateNote = (id, content) =>
+  api.put(`/notes/${id}`, { content });
+
+export const deleteNote = (id) =>
+  api.delete(`/notes/${id}`);
